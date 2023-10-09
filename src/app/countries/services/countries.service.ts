d13bbeb0de5d131f348c2a7dda8627b367c7ae81
Alberto Gómez <alberto.grodriguez@bosonit.com>
2023-10-09 14:34:59 +0200

    Finalizada seccion 9

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, of, map } from 'rxjs';
 
 import { Country } from '../interfaces/country';
 
@@ -9,10 +9,11 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1/';
   constructor(private http: HttpClient) {}
 
-  searchCountryByAlphaCode(code: string): Observable<Country[]>{
+  searchCountryByAlphaCode(code: string): Observable<Country|null>{
     const url = `${this.apiUrl}/alpha/${code}`;
     return this.http.get<Country[]>(url).pipe(
-      catchError(error => of([]))
+      map(countries=> countries.length>0 ? countries[0]:null),
+      catchError(error => of(null))
     )
   }
 
